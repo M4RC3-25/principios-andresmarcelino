ca6edf83ffaee58c49b251d356281d8df995d89c
M4RC3-25 <amescobar250306@gmail.com>
2026-02-12 14:51:41 -0600

    Ejercicio 1

---
+++ Ejercicio-01/Main.java
@@ -0,0 +1,41 @@
+
+
+/**
+ * Class Main
+ */
+public class Main {
+
+  //
+  // Fields
+  //
+
+  
+  //
+  // Constructors
+  //
+  public Main () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  //
+  // Other methods
+  //
+
+  /**
+   * @param        args void
+   */
+  public static void main(String[] args)
+  {
+  Persona p = new Persona("andres", 19);
+  p.saludar();
+  }
+
+
+}
